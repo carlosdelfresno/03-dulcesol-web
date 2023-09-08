48e11ab79f7974f48f988aba2465fac021200c52
Carlos del Fresno <cdelfresno@iti.es>
2023-09-08 11:09:01 +0200

    Primer commit

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export interface Producto {
+  id: number
+  nombre: string
+  categoria: string
+  subcategoria: string
+  imageThumbnail: string
+  imagen: string
+  valorEnergetico: string
+  unidadesPaquete: string
+  pesoPaquete: string
+  descripcionCorta: string
+  descripcion: string
+  ingredientes: string
+}
